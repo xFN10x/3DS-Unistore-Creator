987577a4db5a3fa35f2ac06aa3b66f4019ef83a6
_FN10_ <mathdudex09@gmail.com>
2025-09-29 11:36:20 -0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation(libs.guava)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "fn10.unistoreCreator.Launcher"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ src/main/java/fn10/unistoreCreator/Launcher.java
@@ -0,0 +1,8 @@
+package fn10.unistoreCreator;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        
+    }
+}
